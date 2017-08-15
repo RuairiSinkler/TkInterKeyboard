5ea670b61659e6e6b753ee22fa1b5565d70310c0
Ruairi Sinkler <sinky_son@hotmail.co.uk>
2017-08-15 14:48:10 +0100

    Shift and Caps_Lock buttons now work and ability to add "BLANK" spaces ahs been added to better format keyboard

--- TkInterKeyboard.py
+++ TkInterKeyboard.py
@@ -12,8 +12,11 @@ class keyboard():
         self.entry = entry
         self.button_width = button_width
         self.button_height = button_height
+        self.button_values = []
         self.buttons = buttons
         self.alt_buttons = alt_buttons
+        self.shift = False
+        self.caps_lock = False
 
     def build(self):
         i = 0
@@ -21,36 +24,50 @@ class keyboard():
         c = 0
         for row in self.buttons:
             for element in row:
-                value = element
+                value = tkinter.StringVar()
+                value.set(element)
+                self.button_values.append(value)
+                name = value.get()
                 columnspan = 1
                 rowspan = 1
                 width = self.button_width
                 height = self.button_height
-                callback = lambda v=value: self.entry.insert(tkinter.INSERT, v)
-                if value == " ":
-                    columnspan = 5
-                    width = 5 * self.button_width
-                elif value == "SHIFT":
-                    print(c)
-                    columnspan = 2
-                    width = 2 * self.button_width
-                    # callback = TODO: add shift functionality
-                elif value == "ENTER":
-                    columnspan = 2
-                    rowspan = 2
-                    width = 2 * self.button_width
-                    height = 2 * self.button_height
-                    # callback = TODO: add enter functionality
-                elif value == "BACKSPACE":
-                    columnspan = 2
-                    width = 2 * self.button_width
-                    callback = lambda: self.entry.delete(self.entry.index(tkinter.INSERT) - 1)
-                button = tkinter.Button(self.frame, text=value, width=width, height=height, command=callback)
-                button.grid(row=r, column=c, columnspan=columnspan, rowspan=rowspan)
+                if name == "BLANK":
+                    frame = tkinter.Frame(self.frame, width=1, height=1)
+                    frame.grid(row=r, column=c, columnspan=columnspan, rowspan=rowspan)
+                else:
+                    callback = lambda v=value: self.key_press(v)
+                    if name == "SPACE":
+                        value.set(" ")
+                        columnspan = 5
+                        width = 5 * self.button_width
+                    elif name == "CAPS_LOCK":
+                        callback = lambda: self.toggle_caps_lock()
+                    elif name == "SHIFT":
+                        columnspan = 2
+                        width = 2 * self.button_width
+                        callback = lambda: self.toggle_shift()
+                    elif name == "ENTER":
+                        columnspan = 2
+                        rowspan = 2
+                        width = 2 * self.button_width
+                        height = 2 * self.button_height
+                        # callback = TODO: add enter functionality
+                    elif name == "BACKSPACE":
+                        columnspan = 2
+                        width = 2 * self.button_width
+                        callback = lambda: self.entry.delete(self.entry.index(tkinter.INSERT) - 1)
+                    button = tkinter.Button(self.frame, textvariable=value, width=width, height=height, command=callback)
+                    button.grid(row=r, column=c, columnspan=columnspan, rowspan=rowspan)
                 i = i + 1
                 c = c + columnspan
             r = r + 1
             c = 0
+
+    def key_press(self, value):
+        self.entry.insert(tkinter.INSERT, value.get())
+        if self.shift:
+            self.toggle_shift()
 
     def check_values(self, buttons, alt_buttons):
         if not (alt_buttons is None):
@@ -71,18 +88,40 @@ class keyboard():
                         return False
         return True
 
+    def refresh_vars(self):
+        if self.shift != self.caps_lock:
+            values = self.alt_buttons
+        else:
+            values = self.buttons
+        i = 0
+        for row in values:
+            for element in row:
+                self.button_values[i].set(element)
+                i = i + 1
+
+    def toggle_caps_lock(self):
+        self.caps_lock = not(self.caps_lock)
+        self.refresh_vars()
+
+    def toggle_shift(self):
+        self.shift = not(self.shift)
+        self.refresh_vars()
+
+    def set_entry(self, entry):
+        self.entry = entry
+
 def main():
     root = tkinter.Tk()
     e = tkinter.Entry(root)
     e.pack()
     buttons = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "BACKSPACE"],
-               ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ENTER"],
+               ["CAPS_LOCK", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ENTER"],
                ["SHIFT", "z", "x", "c", "v", "b", "n", "m"],
-               [" "]]
+               ["BLANK", "BLANK", "BLANK", "SPACE"]]
     alt_buttons = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "BACKSPACE"],
-                   ["A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER"],
+                   ["CAPS_LOCK", "A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER"],
                    ["SHIFT", "Z", "X", "C", "V", "B", "N", "M"],
-                   [" "]]
+                   ["BLANK", "BLANK", "BLANK", "SPACE"]]
     kb = keyboard(root, e, 10, 5, buttons, alt_buttons)
     kb.build()
     root.mainloop()
